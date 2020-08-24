30ace8ab3e610fc0ae7fd89aec9ec4e4b3adc735
Gregory Casamento <greg.casamento@gmail.com>
2020-08-24 07:12:48 -0400

    Initial Commit

---
+++ D/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  D
+//
+//  Created by Gregory Casamento on 8/24/20.
+//  Copyright © 2020 Open Logic Corporation. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
